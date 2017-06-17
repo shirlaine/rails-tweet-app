0a15aae55f7720db0e75ccebecf90ff1d9ffe333
shirlaine <shirlainecom@gmail.com>
2017-06-17 09:30:00 +0000

    Add flash message

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,16 @@
 class CommentsController < ApplicationController
     def create
        @tweet = Tweet.find(params[:tweet_id]) 
-       @comment = @tweet.comments.build(comment_params).save
+       @comment = @tweet.comments.build(comment_params)
+       
+      if @comment.save
+          message = { notice: "Comment saved!" }
+          
+      else
+          message = { error: @comment.errors.full_messages.to_sentence}
+      end
     
-    redirect_to tweet_path(@tweet)
+    redirect_to tweet_path(@tweet), flash: message
     end
     
     
